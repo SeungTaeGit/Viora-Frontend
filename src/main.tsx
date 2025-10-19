85cada74c5153a904fa5fcb892d9c47d1cd7d690
SeungTae <dltmdxo1589@daum.net>
2025-10-19 23:00:26 +0900

    Main

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+// src/main.tsx
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'; // 1. BrowserRouter import
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* 2. <App />을 <BrowserRouter>로 감싸줍니다. */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
